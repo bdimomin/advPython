761e70deb112fa986a9e76318a7a632262d081bd
bdimomin <imomin126@gmail.com>
2023-06-06 18:00:02 +0600

    Updated Animal File

--- animal.py
+++ animal.py
@@ -1,50 +1,47 @@
 class Animal():
-    def __init__(self,species):
-       self.species = species
 
+    def __init__(self,color):
+       self.color = color
+
+    def animalColor(self):
+        print("Animal color: ",self.color)
     def sleep(self):
         print("Animal sleeps during daytime")
     def eat(self):
-        print("Animal eats both grace and meat")
+        print("Animal eats both grace and meat.")
     def height(self):
-        print("Animal has several heights")
+        print("Animal has several heights.")
 
-class Cat(Animal):
-    def __init__(self, species):
-        super().__init__(species)
-       
+class Dog(Animal):
+   
+    def __init__(self, color,gender):
+       super().__init__(color)
+       self.gender =gender
+
+    def dogColor(self):
+        print("The color of the dog is : ", self.color)
+    def dogGender(self):
+        print("It is a {} dog".format(self.gender))
+
+class Horse(Animal):
+    def __init__(self, color,types):
+        super().__init__(color)
+        self.types=types
+
+    def horseTypes(self):
+        print("Its a/an {} type horse".format(self.types))
     def sleep(self):
-        print("Cat sleeps during night time")
-    def eat(self):
-        print("Cat eats fish mostly")
-    def height(self):
-        print("Cat is lower in height")
+        print("Horse sleeps standing.")
 
 class Snake(Animal):
     def __init__(self, species):
         super().__init__(species)
         
-    def sleep(self):
-        print("Snake sleeps during night time")
-    def eat(self):
-        print("Snake eats fish mostly")
-    def height(self):
-        print("Snake is lower in height")
-    def feet(self):
-        print("Snake has no foot")
+dog1 =Dog("Brown","Male")
+dog1.dogColor()
+dog1.dogGender()
+dog1.sleep()
 
-class Horse(Animal):
-    def __init__(self, species):
-        super().__init__(species)
-
-    def sleep(self):
-        print("Horse sleeps standing.")
-    def eat(self):
-        print("Horse eats grace mostly.")
-    def height(self):
-        print("Horse is higher in height.")
-    def feet(self):
-        print("Horse has 4 foots")
-
-cat1 =Cat("Mammal")
-cat1.eat()
+horse1 = Horse("Black","Arabian")
+horse1.horseTypes()
+horse1.sleep()
