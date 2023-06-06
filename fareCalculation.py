beac88e2eeaf33d6ac947ba5edff856953ed1c52
bdimomin <imomin126@gmail.com>
2023-06-06 13:01:54 +0600

    Modified FareCal

--- fareCalculation.py
+++ fareCalculation.py
@@ -1,17 +1,20 @@
 class Vehicle():
-    fare =100
+    
     def __init__(self,seatingCapacity):
         self.seatingCapacity=seatingCapacity
 
     def fareCalculation(self):
-        return f"Fare is : {self.seatingCapacity*self.fare}"
+        return f"Vehicle Fare is : {self.seatingCapacity*100}"
 
 
 class Bus(Vehicle):
-    fare = 110
+    def __init__(self, seatingCapacity):
+        super().__init__(seatingCapacity)
+    def fareCalculation(self):
+        return f"BUS Fare is : {self.seatingCapacity*110}"
     
 
-vehicle1 = Vehicle(40)
-bus1 = Bus(40)
+vehicle1 = Vehicle(50)
+bus1 = Bus(50)
 print(vehicle1.fareCalculation())
 print(bus1.fareCalculation())
